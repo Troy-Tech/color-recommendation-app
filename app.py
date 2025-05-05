7e9966058818f4322dea41bb3dbc04205dfe0b57
Rebecca Troy <142261677+rebecca-troy@users.noreply.github.com>
2025-05-05 08:52:18 -0400

    Update app.py

--- app.py
+++ app.py
@@ -137,4 +137,5 @@ def index():
     return render_template("index.html", colors=None)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
